42efb0b4e91fb6b2aaf2500fe7bfa59c379e5969
kgadams93 <kgadams93@gmail.com>
2016-01-25 09:13:54 +0000

    Update AWS region

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -16,7 +16,7 @@ class Attachment < ActiveRecord::Base
     def upload_to_s3
       s3 = Aws::S3::Resource.new
       tenant_name = Tenant.find(Thread.current[:tenant_id]).name
-      obj = s3.bucket(ENV['AWS_S3_BUCKET']).object("#{tenant_name}/#{upload.original_filename}")
+      obj = s3.bucket(ENV['S3_BUCKET']).object("#{tenant_name}/#{upload.original_filename}")
       obj.upload_file(upload.path, acl:'public-read')
       self.key = obj.public_url
     end
