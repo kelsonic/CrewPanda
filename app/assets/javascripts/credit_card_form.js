87f5a2e81cc339665f322eb48c77affcf65481d6
kgadams93 <kgadams93@gmail.com>
2016-01-26 18:01:43 +0000

    Create signup form credit card fields and start credit card form js file

---
+++ app/assets/javascripts/credit_card_form.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  
+  // function to get params from URL
+  
+  // function to handle the submit of the form and intercept the default event
+  
+  // initiate submit handler listener for any form with class cc_form
+  
+  // handle event of plan drop down changing
+  
+  // setup plan change event listener for #tenant_plan id in the forms for class cc_form
+  
+  // call plan change handler so that the plan is set correctly in the drop down when the page loads
+  
+  // funtion to handle the token received from Stripe and remove credit card fields
+  
+  // function to show errors when Stripe functionality returns an error
+  
+});
