a899dd3e0e5714224a9131adb021ad0a267e423b
kgadams93 <kgadams93@gmail.com>
2016-01-28 20:58:35 +0000

    Add project user association

--- app/models/project.rb
+++ app/models/project.rb
@@ -25,7 +25,7 @@ class Project < ActiveRecord::Base
       if user.is_admin?
         tenant.projects.order(:id).limit(1)
       else
-        user.projects_where(tenant_id: tenant.id).order(:id).limit(1)
+        user.projects.where(tenant_id: tenant.id).order(:id).limit(1)
       end
     end
   end
